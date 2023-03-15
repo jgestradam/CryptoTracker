0cfce774c422451d431b78b6ea51fbfc9b31174a
jgestradam <jgestradam@unal.edu.co>
2023-03-14 20:43:37 -0500

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Joseph Estrada on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
