7690574dd26c23f9683f29eb85de10a6db8a7b2a
jgestradam <jgestradam@unal.edu.co>
2023-03-15 14:23:12 -0500

    Creating HomeView and a navigation header

---
+++ CryptoTracker/Core /Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,29 @@
+//
+//  CircleButtonAnimationView.swift
+//  CryptoTracker
+//
+//  Created by Joseph Estrada on 3/15/23.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    
+    @Binding var animate: Bool
+    
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0)
+            .animation(animate ? Animation.easeOut(duration: 0.8) : .none)
+    }
+}
+
+struct CircleButtonAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonAnimationView(animate: .constant(false))
+            .foregroundColor(.red)
+            .frame(width: 100, height: 100)
+    }
+}
