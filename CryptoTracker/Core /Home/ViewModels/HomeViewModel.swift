ffc131d41197abed2554be73fa1ecb88625a66a7
jgestradam <jgestradam@unal.edu.co>
2023-03-16 18:51:18 -0500

    Add a dynamic List to display downloaded coins

---
+++ CryptoTracker/Core /Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewModel.swift
+//  CryptoTracker
+//
+//  Created by Joseph Estrada on 3/16/23.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+
+        }
+    }
+    
+}
